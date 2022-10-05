a126186d9c1acb6ad68f5902ba8dd98b3166511f
Marius Hauser <mariushauser@outlook.de>
2022-10-05 15:35:53 +0200

    add basic pattern import from json files

--- src/components/PatternList.vue
+++ src/components/PatternList.vue
@@ -1,9 +1,42 @@
 <template>
-  <EasyDataTable :headers="headers" :items="items" />
+  <EasyDataTable ref="table" :headers="headers" :items="items" alternating />
 </template>
 
 <script>
 import { defineComponent } from "vue";
+
+function readJsonFile(file, callback) {
+  var request = new XMLHttpRequest();
+  request.overrideMimeType("application/json");
+  request.open("GET", file, true);
+  request.onreadystatechange = function () {
+    if (request.readyState === 4 && request.status == "200") {
+      callback(request.responseText);
+    }
+  };
+  request.send(null);
+}
+
+var patterns = [];
+function getPatterns() {
+  // TODO: currently sorting table is needed to show all patterns
+  readJsonFile("src/patterns/patterns.json", function (text) {
+    var data = JSON.parse(text);
+    console.log(data["patterns"]);
+    for (var pattern in data["patterns"]) {
+      console.log("getting: src/patterns/" + data["patterns"][pattern]);
+      readJsonFile(
+        "src/patterns/" + data["patterns"][pattern],
+        function (text) {
+          var data = JSON.parse(text);
+          patterns.push(data);
+        }
+      );
+    }
+  });
+  return patterns;
+}
+
 export default defineComponent({
   name: "PatternList",
   setup() {
@@ -13,15 +46,7 @@ export default defineComponent({
       { text: "Problem / Motivation", value: "problem", sortable: false },
       { text: "Solution / Application", value: "solution", sortable: false },
     ];
-
-    const items = [
-      {
-        name: "pattern.name",
-        aka: "pattern.aka",
-        problem: "pattern.problem",
-        solution: "pattern.solution",
-      },
-    ];
+    var items = getPatterns();
 
     return {
       headers,
