f07ed0d987d20fa6a42e1f087e401859197afb39
Marius Hauser <mariushauser@outlook.de>
2022-10-27 17:02:15 +0200

    gh pages update

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import vuetify from "vite-plugin-vuetify";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   plugins: [vue(), vuetify({ autoImport: true })],
   resolve: {
     alias: {
